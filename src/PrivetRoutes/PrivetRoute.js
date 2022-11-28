56e72eef90e1ffcd6a3bb1ed93b3b59b57897ef4
Shaim2373 <shaimrahman2368@gmail.com>
2022-11-28 12:51:37 +0600

    some problem solve

--- src/PrivetRoutes/PrivetRoute.js
+++ src/PrivetRoutes/PrivetRoute.js
@@ -6,16 +6,11 @@ import { AuthContext } from '../Contexts/UserContext';
 const PrivetRoute = ({ children }) => {
     const { user, loding } = useContext(AuthContext)
     const location = useLocation()
-    // console.log(location);
-    //loding condition
     if (loding) {
-        // console.log("Loding runing");
         return <div className="flex items-center justify-center space-x-1 ">
             <progress className="progress h-1 progress-secondary"></progress>
         </div>
     }
-
-    //   console.log(user, user?.uid)
     if (user && user?.uid) {
         return children;
     }
