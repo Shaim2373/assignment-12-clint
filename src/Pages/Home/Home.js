1b3b3586f5f4fd6c87677173b8cbb138769e6414
Shaim2373 <shaimrahman2368@gmail.com>
2022-11-28 12:50:25 +0600

    some problem solve

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import { FaArrowRight, FaGoogleWallet, FaSpaceShuttle } from 'react-icons/fa';
 import './home.css'
 import useTitle from '../../hook/useTitle';
-import SliderBanner from './SliderBanner';
 import UserBooking from './AllBooking/UserBooking';
 import AllProductImg from './All product img/AllProductImg';
 import Revewsproduct from './reviews/Reviewsproduct';
@@ -31,21 +30,6 @@ const Home = () => {
 
     return (
         <>
-            <section className='mb-0 h-[auto] mt-[62px] bg-gray-800 md:pb-5'>
-                <div className="carousel w-full bg-gray-500">
-                    {
-                        bannerImage?.map((item, i) => <SliderBanner key={i} item={item}></SliderBanner>)
-                    }
-
-                </div>
-
-
-                <div className="relative w-full flex gap-4 py-6 overflow-x-auto">
-                    {
-                        sliderImag?.map((img, i) => <img key={i} className="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500" src={img?.img} alt="Image 1" />)
-                    }
-                </div>
-            </section>
 
             {/* ----------------All Car Booking section----------------- */}
 
